cb4cf5801260c501f1f5e0e469234b40a49ff6bf
Carlos Enriquez <c3sar64@gmail.com>
2026-01-20 08:40:55 -0400

    Allow all CORS origins for cloud deployment

--- rrhh_system/backend/rrhh_backend/settings.py
+++ rrhh_system/backend/rrhh_backend/settings.py
@@ -131,10 +131,11 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-    'http://127.0.0.1:5173',
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:5173',
+#     'http://127.0.0.1:5173',
+# ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
